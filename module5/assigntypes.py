2672f123a651a7787ceb363585bf7cea80636d56
Dhananjay Singh <djwid92@gmail.com>
2017-09-14 20:45:07 -0700

    Added assignment types

---
+++ module5/assigntypes.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+
+tuple = (78,'abc',56.7, 'efg')
+tinytuple = (123,'pqr')
+
+print(tuple)
+print(tuple[0])
+print(tuple[1:3])
+print(tuple[2:])
+print(tuple*2)
+print(tuple+tinytuple)
