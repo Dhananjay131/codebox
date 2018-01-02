41e05a700207a84253f06af07dd3e5690699a94c
Dhananjay131 <djwid92@gmail.com>
2018-01-02 10:45:01 -0800

    Update editDistance.py

--- editDistance.py
+++ editDistance.py
@@ -5,15 +5,27 @@ class Solution:
         :type word2: str
         :rtype: int
         """
-        m=len(word1)+1
-        n=len(word2)+1
+        len_1=len(word1)
 
-        tbl = {}
-        for i in range(m): tbl[i,0]=i
-        for j in range(n): tbl[0,j]=j
-        for i in range(1, m):
-            for j in range(1, n):
-                cost = 0 if word1[i-1] == word2[j-1] else 1
-                tbl[i,j] = min(tbl[i, j-1]+1, tbl[i-1, j]+1, tbl[i-1, j-1]+cost)
-                
-        return tbl[i,j]
+        len_2=len(word2)
+
+        x =[[0]*(len_2+1) for _ in range(len_1+1)]#the matrix whose last element ->edit distance
+
+        for i in range(0,len_1+1): #initialization of base case values
+
+            x[i][0]=i
+        for j in range(0,len_2+1):
+
+            x[0][j]=j
+        for i in range (1,len_1+1):
+
+            for j in range(1,len_2+1):
+
+                if word1[i-1]==word2[j-1]:
+                    x[i][j] = x[i-1][j-1] 
+
+                else :
+                    x[i][j]= min(x[i][j-1],x[i-1][j],x[i-1][j-1])+1
+
+        print(x)
+        return x[i][j]
