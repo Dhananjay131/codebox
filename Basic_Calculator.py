d8c9a422d578d2397bb2982124147d929582e537
Dhananjay131 <djwid92@gmail.com>
2018-01-03 11:53:42 -0800

    Updated Calculator beating 100% runtime
    runtime beats 100% of all submissions

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -4,32 +4,39 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        stack = []
-        result = 0;
-        number = 0;
-        sign = 1;
+        stack=[]
+        result=0
+        number=0
+        sign=1
+        
         for i in range(len(s)):
-            c = s[i]
+            c=s[i]
             if c.isdigit():
-                number = 10 * number + int(c)
-            elif(c == '+'):
-                result += sign * number
-                number = 0
-                sign = 1
-            elif(c == '-'):
-                result += sign * number
-                number = 0
-                sign = -1
-            elif(c == '('):
+                number= 10*number + int(c)
+            elif c=="+":
+                result+= sign*number
+                number=0
+                sign=1
+            elif c=="-":
+                result+= sign*number
+                number=0
+                sign=-1
+            elif c=="(":
                 stack.append(result)
                 stack.append(sign)
-                sign = 1
-                result = 0
-            elif(c == ')'):
-                result += sign * number 
-                number = 0
-                result *= stack.pop()    
-                result += stack.pop()           
-     
-        if(number != 0): result += sign * number
+                result=0
+                number=0
+                sign=1
+                
+            elif c==")":
+                result+=sign* number
+                result*=stack.pop()
+                result+= stack.pop()
+                number=0
+                sign=1
+                
+            
+        if(number!=0): 
+            result+= sign*number
+            
         return result
