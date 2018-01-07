e30ca82c2d5c3ec58b9e33d13be565abfdf180d3
Dhananjay131 <djwid92@gmail.com>
2018-01-06 23:33:21 -0800

    Created First Unique Character in a String
    find the first non-repeating character in it and return it's index, otherwise -1.

---
+++ FirstUniqueCharacterinString.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        letters='abcdefghijklmnopqrstuvwxyz'
+        index=[s.index(l) for l in letters if s.count(l) == 1]
+        print(index)
+        return min(index) if len(index) > 0 else -1
