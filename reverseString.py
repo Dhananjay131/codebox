ec6217b496e10d57a598af19ea7255f373459943
Dhananjay131 <djwid92@gmail.com>
2018-01-07 20:18:46 -0800

    Update reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -5,3 +5,5 @@ class Solution:
         :rtype: str
         """
         return s[::-1]
+    #''.join(reversed('a string')) this works as well. 
+    # *Strings are immuatable in python. Changing a string does not modify the string. It creates a new one.
