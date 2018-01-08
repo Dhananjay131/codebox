703e09cfa9ca4e3ee421b4f1d082417fdf1c6d34
Dhananjay131 <djwid92@gmail.com>
2018-01-08 02:08:52 -0800

    Created MajorityElement.py
    Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
    
    You may assume that the array is non-empty and the majority element always exist in the array.

---
+++ MajorityElement.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[int(len(nums)/2)]
+         
+        #return sorted(nums)[len(nums)/2]
+        
