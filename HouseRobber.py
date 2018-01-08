7d4dc4820c1274ff66e15d7838855d5dd3886b28
Dhananjay131 <djwid92@gmail.com>
2018-01-08 12:17:37 -0800

    Update HouseRobber.py

--- HouseRobber.py
+++ HouseRobber.py
@@ -3,7 +3,6 @@ class Solution:
     def rob(self, nums):
         last, now = 0, 0
         for k in nums:
-            print(k,last,now, last+k)
             last, now = now, max(last+k,now)
         return(now)
         
